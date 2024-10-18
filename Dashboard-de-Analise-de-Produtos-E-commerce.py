34fd83f8788faf8af8301b236143fcefc177c97a
Diogo <diogocenteno1979@gmail.com>
2024-10-18 11:09:58 -0300

    Update Dashboard-de-Analise-de-Produtos-E-commerce.py

--- Dashboard-de-Analise-de-Produtos-E-commerce.py
+++ Dashboard-de-Analise-de-Produtos-E-commerce.py
@@ -3,47 +3,46 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
 
-# Tente carregar os dados
-try:
-    df = pd.read_csv('C:/Users/diogo/Downloads/ecommerce_estatistica.csv')  # Carrega o arquivo CSV
-    print(df.head())  # Exibe as primeiras linhas do DataFrame
-    print(df.info())  # Exibe informações sobre as colunas e tipos de dados
-    print(df.describe())  # Exibe estatísticas descritivas das colunas numéricas
-    pd.set_option('display.max_columns', None)  # Configura o Pandas para mostrar todas as colunas
-    print(df)  # Exibe o DataFrame completo
-    print(df.isnull().sum())  # Verifica e exibe a contagem de valores nulos em cada coluna
-except Exception as e:
-    print(f"Erro ao carregar os dados: {e}")  # Mensagem de erro caso falhe ao carregar os dados
+# Função para carregar os dados de um arquivo CSV
+def carregar_dados(caminho):
+    """Carrega os dados de um arquivo CSV e retorna um DataFrame."""
+    try:
+        # Lê o CSV e exibe as primeiras linhas, informações e estatísticas
+        df = pd.read_csv(caminho)
+        print(df.head())
+        print(df.info())
+        print(df.describe())
+        pd.set_option('display.max_columns', None)  # Exibe todas as colunas
+        print(df)
+        print(df.isnull().sum())  # Verifica valores ausentes
+        return df
+    except Exception as e:
+        print(f"Erro ao carregar os dados: {e}")
+        return pd.DataFrame()  # Retorna um DataFrame vazio em caso de erro
 
+# Função para criar gráficos a partir do DataFrame
 def cria_graficos(df):
+    """Cria gráficos a partir do DataFrame fornecido e retorna uma lista de figuras e marcas populares."""
     # Filtra as 15 marcas mais populares
     top_brands = df['Marca'].value_counts().nlargest(15).index
-    df_filtered = df[df['Marca'].isin(top_brands)]  # Filtra o DataFrame para incluir apenas as marcas populares
+    df_filtered = df[df['Marca'].isin(top_brands)]  # Filtra o DataFrame para as top marcas
 
-    # Cria gráfico de histograma da nota dos produtos
+    # Cria os gráficos
     fig1 = px.histogram(df_filtered, x='Nota', nbins=30, title='Distribuição da Nota dos Produtos')
-    fig1.update_layout(xaxis_title='Nota', yaxis_title='Contagem', legend_title='Distribuição')
-
-    # Cria gráfico de pizza para distribuição de marcas
     fig2 = px.pie(df_filtered, names='Marca', values='N_Avaliações', title='Distribuição de Marcas', hole=0.1)
-    fig2.update_layout(height=600, width=900)
-
-    # Cria gráfico de dispersão: Nota vs Preço
     fig3 = px.scatter(df_filtered, x='Preço', y='Nota', color='Marca', hover_name='Título', size_max=20)
-    fig3.update_layout(title='Nota vs. Preço dos Produtos', xaxis_title='Preço', yaxis_title='Nota', legend_title='Marca')
-
-    # Cria gráfico de linha: Nota por Desconto
     fig4 = px.line(df_filtered, x='Desconto', y='Nota', color='Marca', title='Nota por Desconto')
-    fig4.update_layout(xaxis_title='Desconto', yaxis_title='Nota', legend_title='Marca')
-
-    # Cria gráfico de barras: Quantidade de Avaliações por Marca
     fig5 = px.bar(df_filtered, x='Título', y='N_Avaliações', title='Quantidade de Avaliações por Marca')
-    fig5.update_layout(height=800, width=1200, xaxis_title='Título', yaxis_title='N_Avaliações', legend_title='Marca')
 
-    return fig1, fig2, fig3, fig4, fig5, top_brands  # Retorna todos os gráficos e as marcas populares
+    # Ajusta o layout dos gráficos
+    fig2.update_layout(height=600, width=900)
+    fig5.update_layout(height=800, width=1200)
 
+    return fig1, fig2, fig3, fig4, fig5, top_brands  # Retorna as figuras e marcas populares
+
+# Função para criar um gráfico 3D
 def cria_grafico_3d(filtered_df):
-    # Cria um gráfico 3D: Preço vs Nota vs Desconto
+    """Cria um gráfico 3D a partir do DataFrame filtrado."""
     fig_3d = go.Figure(data=[go.Scatter3d(
         x=filtered_df['Preço'],
         y=filtered_df['Nota'],
@@ -51,134 +50,225 @@ def cria_grafico_3d(filtered_df):
         mode='markers',
         marker=dict(
             size=12,
-            color=filtered_df['N_Avaliações'],  # Cor baseada no número de avaliações
+            color=filtered_df['N_Avaliações'],
             colorscale='Viridis',
             colorbar=dict(title='N_Avaliações'),
             showscale=True
         ),
-        text=filtered_df['Título']  # Mostra o título do produto ao passar o mouse
+        text=filtered_df['Título']
     )])
 
+    # Configura os eixos do gráfico 3D
     fig_3d.update_layout(title='Gráfico 3D: Preço vs. Nota vs. Desconto',
                          scene=dict(
                              xaxis_title='Preço',
                              yaxis_title='Nota',
                              zaxis_title='Desconto'
                          ))
-    return fig_3d  # Retorna o gráfico 3D
+    return fig_3d
 
+# Função para criar e configurar a aplicação Dash
 def cria_app(df):
-    app = Dash(__name__)  # Cria uma instância do aplicativo Dash
+    """Cria e configura a aplicação Dash."""
+    app = Dash(__name__)  # Inicializa a aplicação
 
-    # Gera os gráficos iniciais
+    # Cria os gráficos iniciais
     fig1, fig2, fig3, fig4, fig5, top_brands = cria_graficos(df)
 
     # Define o layout da aplicação
-    app.layout = html.Div([
-        html.H1("Análise de Produtos E-commerce", style={'textAlign': 'center'}),  # Título do aplicativo
+    app.layout = html.Div(style={'backgroundColor': '#f0f0f0', 'padding': '20px'}, children=[
+        html.H1("Análise de Produtos E-commerce", style={'textAlign': 'center', 'color': '#333'}),
 
-        html.Div([
-            dcc.Graph(figure=fig1, id='histogram-plot'),  # Gráfico de histograma
-            dcc.Dropdown(
-                id='histogram-dropdown',
-                options=[{'label': brand, 'value': brand} for brand in top_brands],  # Opções do dropdown
-                value=top_brands[0],  # Valor padrão
-                clearable=False,
-                style={'width': '50%', 'margin': '0 auto'}
-            ),
-            dcc.Graph(figure=fig2),  # Gráfico de pizza
-            dcc.Graph(id='scatter-plot'),  # Gráfico de dispersão
-            dcc.Dropdown(
-                id='scatter-dropdown',
-                options=[{'label': brand, 'value': brand} for brand in top_brands],  # Opções do dropdown
-                value=top_brands[0],  # Valor padrão
-                clearable=False,
-                style={'width': '50%', 'margin': '0 auto'}
-            ),
-            dcc.Graph(id='line-chart'),  # Gráfico de linha
-            dcc.Dropdown(
-                id='line-dropdown',
-                options=[{'label': brand, 'value': brand} for brand in top_brands],  # Opções do dropdown
-                value=top_brands[0],  # Valor padrão
-                clearable=False,
-                style={'width': '50%', 'margin': '0 auto'}
-            ),
-            dcc.Graph(id='bar-chart'),  # Gráfico de barras
-            dcc.Dropdown(
-                id='bar-dropdown',
-                options=[{'label': brand, 'value': brand} for brand in top_brands],  # Opções do dropdown
-                value=top_brands[0],  # Valor padrão
-                clearable=False,
-                style={'width': '50%', 'margin': '0 auto'}
-            ),
-            dcc.Graph(id='3d-scatter'),  # Gráfico 3D
-        ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'})  # Estilização do layout
+        # Sliders para filtrar dados
+        html.Div(className='row', children=[
+            # Slider para faixa de preço
+            html.Div(className='four columns', children=[
+                html.Label("Faixa de Preço"),
+                dcc.RangeSlider(
+                    id='preco-slider',
+                    min=df['Preço'].min(),
+                    max=df['Preço'].max(),
+                    value=[df['Preço'].min(), df['Preço'].max()],
+                    marks={i: f'R${i}' for i in range(int(df['Preço'].min()), int(df['Preço'].max()) + 1, 50)},
+                    step=1,
+                    tooltip={"placement": "bottom", "always_visible": True}
+                ),
+                html.Div(id='preco-output', style={'textAlign': 'center'})
+            ]),
+            # Slider para faixa de nota
+            html.Div(className='four columns', children=[
+                html.Label("Faixa de Nota"),
+                dcc.RangeSlider(
+                    id='nota-slider',
+                    min=df['Nota'].min(),
+                    max=df['Nota'].max(),
+                    value=[df['Nota'].min(), df['Nota'].max()],
+                    marks={i: str(i) for i in range(int(df['Nota'].min()), int(df['Nota'].max()) + 1)},
+                    step=0.1,
+                    tooltip={"placement": "bottom", "always_visible": True}
+                ),
+                html.Div(id='nota-output', style={'textAlign': 'center'})
+            ]),
+            # Slider para faixa de desconto
+            html.Div(className='four columns', children=[
+                html.Label("Faixa de Desconto"),
+                dcc.RangeSlider(
+                    id='desconto-slider',
+                    min=df['Desconto'].min(),
+                    max=df['Desconto'].max(),
+                    value=[df['Desconto'].min(), df['Desconto'].max()],
+                    marks={i: str(i) for i in range(int(df['Desconto'].min()), int(df['Desconto'].max()) + 1)},
+                    step=1,
+                    tooltip={"placement": "bottom", "always_visible": True}
+                ),
+                html.Div(id='desconto-output', style={'textAlign': 'center'})
+            ]),
+        ]),
+
+        # Dropdowns para filtrar por gênero e material
+        html.Div(className='row', children=[
+            html.Div(className='four columns', children=[
+                html.Label("Gênero"),
+                dcc.Dropdown(
+                    id='genero-dropdown',
+                    options=[{'label': genero, 'value': genero} for genero in df['Gênero'].unique()],
+                    multi=True,
+                    placeholder='Selecione um ou mais gêneros'
+                )
+            ]),
+            html.Div(className='four columns', children=[
+                html.Label("Material"),
+                dcc.Dropdown(
+                    id='material-dropdown',
+                    options=[{'label': material, 'value': material} for material in df['Material'].unique()],
+                    multi=True,
+                    placeholder='Selecione um ou mais materiais'
+                )
+            ]),
+        ]),
+
+        # Gráficos abaixo dos filtros
+        html.Div(className='row', children=[
+            html.Div(className='four columns', children=[
+                dcc.Dropdown(id='histogram-dropdown',
+                             options=[{'label': brand, 'value': brand} for brand in top_brands],
+                             value=top_brands[0],
+                             clearable=False,
+                             style={'margin-top': '10px'}),
+                dcc.Graph(figure=fig1, id='histogram-plot')
+            ]),
+            html.Div(className='four columns', children=[
+                dcc.Graph(figure=fig2),
+            ]),
+            html.Div(className='four columns', children=[
+                dcc.Dropdown(id='scatter-dropdown',
+                             options=[{'label': brand, 'value': brand} for brand in top_brands],
+                             value=top_brands[0],
+                             clearable=False,
+                             style={'margin-top': '10px'}),
+                dcc.Graph(id='scatter-plot'),
+            ]),
+        ]),
+
+        html.Div(className='row', children=[
+            html.Div(className='four columns', children=[
+                dcc.Dropdown(id='line-dropdown',
+                             options=[{'label': brand, 'value': brand} for brand in top_brands],
+                             value=top_brands[0],
+                             clearable=False,
+                             style={'margin-top': '10px'}),
+                dcc.Graph(id='line-chart'),
+            ]),
+            html.Div(className='four columns', children=[
+                dcc.Dropdown(id='bar-dropdown',
+                             options=[{'label': brand, 'value': brand} for brand in top_brands],
+                             value=top_brands[0],
+                             clearable=False,
+                             style={'margin-top': '10px'}),
+                dcc.Graph(id='bar-chart'),
+            ]),
+            html.Div(className='four columns', children=[
+                dcc.Graph(id='3d-scatter'),
+            ]),
+        ]),
     ])
 
-    # Define os callbacks para atualizar os gráficos com base na seleção do usuário
+    # Define os callbacks para atualizar os gráficos e os valores dos sliders
     @app.callback(
         [Output('scatter-plot', 'figure'),
          Output('line-chart', 'figure'),
          Output('bar-chart', 'figure'),
          Output('3d-scatter', 'figure'),
-         Output('histogram-plot', 'figure')],
+         Output('histogram-plot', 'figure'),
+         Output('preco-output', 'children'),
+         Output('nota-output', 'children'),
+         Output('desconto-output', 'children')],
         [Input('scatter-dropdown', 'value'),
          Input('line-dropdown', 'value'),
          Input('bar-dropdown', 'value'),
-         Input('histogram-dropdown', 'value')]
+         Input('histogram-dropdown', 'value'),
+         Input('preco-slider', 'value'),
+         Input('nota-slider', 'value'),
+         Input('desconto-slider', 'value'),
+         Input('genero-dropdown', 'value'),
+         Input('material-dropdown', 'value')]
     )
-    def update_graphs(scatter_brand, line_brand, bar_brand, histogram_brand):
-        # Filtra os dados com base na marca selecionada em cada gráfico
-        filtered_scatter = df[df['Marca'] == scatter_brand]
+    def update_graphs(scatter_brand, line_brand, bar_brand, histogram_brand, preco_range, nota_range, desconto_range, generos_selecionados, materiais_selecionados):
+        # Filtra os dados com base na marca selecionada e nos sliders
+        filtered_df = df[
+            (df['Marca'] == scatter_brand) &
+            (df['Preço'] >= preco_range[0]) & (df['Preço'] <= preco_range[1]) &
+            (df['Nota'] >= nota_range[0]) & (df['Nota'] <= nota_range[1]) &
+            (df['Desconto'] >= desconto_range[0]) & (df['Desconto'] <= desconto_range[1])
+        ]
+
+        # Aplica filtros de gênero e material
+        if generos_selecionados:
+            filtered_df = filtered_df[filtered_df['Gênero'].isin(generos_selecionados)]
+        if materiais_selecionados:
+            filtered_df = filtered_df[filtered_df['Material'].isin(materiais_selecionados)]
+
+        # Filtra o DataFrame para o histograma
+        filtered_histogram = df[
+            (df['Marca'] == histogram_brand) &
+            (df['Preço'] >= preco_range[0]) & (df['Preço'] <= preco_range[1]) &
+            (df['Nota'] >= nota_range[0]) & (df['Nota'] <= nota_range[1]) &
+            (df['Desconto'] >= desconto_range[0]) & (df['Desconto'] <= desconto_range[1])
+        ]
+
+        # Aplica filtros de gênero e material ao histograma
+        filtered_histogram = filtered_histogram[
+            (filtered_histogram['Gênero'].isin(generos_selecionados)) &
+            (filtered_histogram['Material'].isin(materiais_selecionados))
+        ] if generos_selecionados and materiais_selecionados else filtered_histogram
+
+        # Filtra o DataFrame para o gráfico de linha
         filtered_line = df[df['Marca'] == line_brand]
-        filtered_bar = df[df['Marca'] == bar_brand]
-        filtered_histogram = df[df['Marca'] == histogram_brand]
 
-        # Atualiza o gráfico de dispersão
-        scatter_fig = px.scatter(
-            filtered_scatter,
-            x='Preço',
-            y='Nota',
-            color='Marca',
-            hover_name='Título',
-            title=f'Nota vs. Preço - {scatter_brand}'  # Título dinâmico
-        )
+        # Cria os gráficos atualizados
+        scatter_fig = px.scatter(filtered_df, x='Preço', y='Nota', color='Marca', hover_name='Título',
+                                 title=f'Nota vs. Preço - {scatter_brand}')
+        line_fig = px.line(filtered_line, x='Desconto', y='Nota', color='Marca',
+                           title=f'Nota por Desconto - {line_brand}')
+        bar_fig = px.bar(filtered_df, x='Título', y='N_Avaliações', title=f'Quantidade de Avaliações - {bar_brand}')
+        fig_3d = cria_grafico_3d(filtered_df)
+        histogram_fig = px.histogram(filtered_histogram, x='Nota', nbins=30,
+                                     title=f'Distribuição da Nota - {histogram_brand}')
 
-        # Atualiza o gráfico de linha
-        line_fig = px.line(
-            filtered_line,
-            x='Desconto',
-            y='Nota',
-            color='Marca',
-            title=f'Nota por Desconto - {line_brand}'  # Título dinâmico
-        )
+        # Atualiza os rótulos dos sliders
+        preco_label = f'Faixa de Preço: R${preco_range[0]} - R${preco_range[1]}'
+        nota_label = f'Faixa de Nota: {nota_range[0]} - {nota_range[1]}'
+        desconto_label = f'Faixa de Desconto: {desconto_range[0]}% - {desconto_range[1]}%'
 
-        # Atualiza o gráfico de barras
-        bar_fig = px.bar(
-            filtered_bar,
-            x='Título',
-            y='N_Avaliações',
-            title=f'Quantidade de Avaliações - {bar_brand}'  # Título dinâmico
-        )
+        return scatter_fig, line_fig, bar_fig, fig_3d, histogram_fig, preco_label, nota_label, desconto_label
 
-        # Atualiza o gráfico 3D com os dados filtrados
-        fig_3d = cria_grafico_3d(filtered_bar)
+    return app  # Retorna a aplicação Dash
 
-        # Atualiza o gráfico de histograma
-        histogram_fig = px.histogram(
-            filtered_histogram,
-            x='Nota',
-            nbins=30,
-            title=f'Distribuição da Nota - {histogram_brand}'  # Título dinâmico
-        )
-
-        return scatter_fig, line_fig, bar_fig, fig_3d, histogram_fig  # Retorna os gráficos atualizados
-
-    return app  # Retorna a instância do aplicativo
-
-# Cria a instância do aplicativo
-app = cria_app(df)
+# Carrega os dados e cria a instância do aplicativo
+caminho_dados = 'C:/Users/diogo/Downloads/ecommerce_estatistica.csv'
+df = carregar_dados(caminho_dados)  # Carrega os dados
+app = cria_app(df)  # Cria a aplicação
 
 # Executa o aplicativo
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)  # Inicia o servidor do Dash
+    app.run_server(debug=True, port=8050)  # Inicia o servidor
